0b45c488a8d1c42827c944babb2f1702b8c34780
nuaayxy <xye@g.clemson.edu>
2022-01-07 01:40:48 -0800

    a

--- model.py
+++ model.py
@@ -11,6 +11,7 @@ import cv2
 import numpy as np
 from numpy import ceil
 import sklearn
+from scipy import ndimage
 # import np.random.shuffle as shuffle
 
 
@@ -27,6 +28,7 @@ def generator(samples, batch_size=32):
             for batch_sample in batch_samples:
                 name = './IMG/'+batch_sample[0].split('/')[-1]
                 center_image = cv2.imread(name)
+                center_image = ndimage.imread(name)
                 center_angle = float(batch_sample[3])
                 images.append(center_image)
                 angles.append(center_angle)
@@ -83,13 +85,12 @@ ch, row, col = 3, 160, 320  # Trimmed image format
 
 model = Sequential()
 # Preprocess incoming data, centered around zero with small standard deviation 
-model.add(Lambda(lambda x: x/255.0 - 0.5,
-        input_shape=(ch, row, col),
-        output_shape=(ch, row, col)))
+model.add(Lambda(lambda x: x/255.0 - 0.5,input_shape=(row, col, ch)))
 
 #model.add(... finish defining the rest of your model architecture here ...)
 model.add(Cropping2D(cropping=((70,25),(0,0))))
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation = 'relu'))
+model.add(Convolution2D(36,5,5,subsample=(2,2), activation='relu'))
 model.add(Convolution2D(48,5,5,subsample=(2,2), activation='relu'))
 model.add(Convolution2D(64,3,3,subsample=(2,2), activation='relu'))
 model.add(Convolution2D(64,3,3,subsample=(2,2), activation='relu'))
@@ -97,6 +98,7 @@ model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
+model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, 
